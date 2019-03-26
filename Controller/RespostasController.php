bfb4ef864a72c85b2e79a3840caf8a7e8a6f271d
Massau <joyce.santos202@etec.sp.gov.br>
2019-03-26 18:02:01 -0300

    Pega registros do banco no Controller

--- Controller/RespostasController.php
+++ Controller/RespostasController.php
@@ -3,10 +3,13 @@ App::uses('AppController', 'Controller');
 
 class RespostasController extends AppController {
 	public function index() {
-		$respostas = array(
+/*		$respostas = array(
 			array('Resposta' => array('nome' => 'Katia', 'idade' => '20')),
 			array('Resposta' => array('nome' => 'Claudio', 'idade' => '36')),
 		);
+*/
+		$respostas = $this->Resposta->find('all');
+
 		$this->set('respostas', $respostas);
 	}
 }
