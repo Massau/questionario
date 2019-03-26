52063ccc945fbe04338bb6d9fb15cad65adc6c9d
Massau <joyce.santos202@etec.sp.gov.br>
2019-03-26 07:47:11 -0300

    Cria arquivo que metadatabase

---
+++ Config/Schema/metadatabase.sql
@@ -0,0 +1,6 @@
+CREATE TABLE respostas (
+	id int(11) NOT NULL AUTO_INCREMENT,
+	nome varchar(100) DEFAULT NULL,
+	idade int(3) DEFAULT NULL,
+	PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
